0793cde2c794f31bba4a4da6bd89d2a296f3e09d
Davi Munhoz <davimunhoz1005@gmail.com>
2023-07-13 22:52:36 -0300

    Replacement position games

--- src/main/java/springintensivao/dslist/services/GameListService.java
+++ src/main/java/springintensivao/dslist/services/GameListService.java
@@ -7,20 +7,41 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import springintensivao.dslist.dto.GameListDTO;
-import springintensivao.dslist.dto.GameMinDTO;
 import springintensivao.dslist.entities.GameList;
+import springintensivao.dslist.projections.GameMinProjection;
 import springintensivao.dslist.repositories.GameListRepository;
+import springintensivao.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;		
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
