7446b074cd985c9b57ec42bee64a868f6bde268c
Davi Munhoz <davimunhoz1005@gmail.com>
2023-07-17 17:02:08 -0300

    Modify request post to patch

--- src/main/java/springintensivao/dslist/controllers/GameListController.java
+++ src/main/java/springintensivao/dslist/controllers/GameListController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +38,7 @@ public class GameListController {
 		return result;		
 	}
 	
-	@PostMapping(value = "/{listId}/games/replacement")
+	@PatchMapping(value = "/{listId}/games/replacement")
 	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
 		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
 	}
